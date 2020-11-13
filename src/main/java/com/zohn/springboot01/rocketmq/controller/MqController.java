2779bcf13d069f8b826aba1e00dbe897b50c294c
zohn <zzohn@foxmail.com>
2020-11-13 10:34:41 +0800

    修改rocketmq服务为阿里云远程部署服务02

--- src/main/java/com/zohn/springboot01/rocketmq/controller/MqController.java
+++ src/main/java/com/zohn/springboot01/rocketmq/controller/MqController.java
@@ -1,10 +1,20 @@
 package com.zohn.springboot01.rocketmq.controller;
 
 
+import com.zohn.springboot01.rocketmq.domain.JsonData;
 import com.zohn.springboot01.rocketmq.service.ProducerService;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
 
 
 @RequestMapping("/rocketmq")
@@ -20,40 +30,28 @@ public class MqController {
 
 
 
+	/**
+	 * 功能描述：微信支付回调接口
+	 * @param msg 支付信息
+	 * @return
+	 */
+	@GetMapping("comment")
+	public Object comment(String msg) throws MQClientException, RemotingException, MQBrokerException, InterruptedException, UnsupportedEncodingException {
+
+		/**
+        * 创建一个消息实例，包含 topic、tag 和 消息体
+       */
+       Message message = new Message("commentTopic","add", msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
+
+       //同步的方式，会有返回结果,发送的是普通消息
+       SendResult result = producer.getProducer().send(message);
+
+       System.out.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+
+       return JsonData.buildSuccess("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+	}
 
 
 
-
-
-
-
-
-
-
-
-//
-//	/**
-//	 * 功能描述：微信支付回调接口
-//	 * @param msg 支付信息
-//	 * @return
-//	 */
-//	@GetMapping("comment")
-//	public Object comment(String msg) throws MQClientException, RemotingException, MQBrokerException, InterruptedException, UnsupportedEncodingException{
-//
-//		/**
-//        * 创建一个消息实例，包含 topic、tag 和 消息体
-//       */
-//       Message message = new Message("commentTopic","add", msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
-//
-//       //同步的方式，会有返回结果,发送的是普通消息
-//       SendResult result = msgProducer.getProducer().send(message);
-//
-//       System.out.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
-//
-//       return JsonData.buildSuccess();
-//	}
-//
-//
-//
 
 }
--- src/main/java/com/zohn/springboot01/rocketmq/service/ProducerService.java
+++ src/main/java/com/zohn/springboot01/rocketmq/service/ProducerService.java
@@ -13,44 +13,48 @@ import javax.annotation.PreDestroy;
 @Service
 public class ProducerService {
 
-        @Value("${apache.rocketmq.producer.producerGroup}")
-        private String producerGroup;
+    @Value("${apache.rocketmq.producer.producerGroup}")
+    private String producerGroup;
 
-        @Value("${apache.rocketmq.namesrvAddr}")
-        private String namesrvAddr;
+    @Value("${apache.rocketmq.namesrvAddr}")
+    private String namesrvAddr;
 
-        private DefaultMQProducer producer;
+    private DefaultMQProducer producer;
 
-        @PostConstruct
-        public void initProducer() {
-            producer = new DefaultMQProducer(producerGroup);
-            producer.setNamesrvAddr(namesrvAddr);
-            producer.setRetryTimesWhenSendFailed(3);
-            try {
-                producer.start();
-                System.out.println("[Producer 已启动]");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public DefaultMQProducer getProducer() {
+        return this.producer;
+    }
+
+    @PostConstruct
+    public void initProducer() {
+        producer = new DefaultMQProducer(producerGroup);
+        producer.setNamesrvAddr(namesrvAddr);
+        producer.setRetryTimesWhenSendFailed(3);
+        try {
+            producer.start();
+            System.out.println("[Producer 已启动]");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        public String send(String topic, String tags, String msg) {
-            SendResult result = null;
-            try {
-                Message message = new Message(topic, tags, msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
-                result = producer.send(message,10000);
-                System.out.println("[Producer] msgID(" + result.getMsgId() + ") " + result.getSendStatus());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public String send(String topic, String tags, String msg) {
+        SendResult result = null;
+        try {
+            Message message = new Message(topic, tags, msg.getBytes(RemotingHelper.DEFAULT_CHARSET));
+            result = producer.send(message, 10000);
+            System.out.println("[Producer] msgID(" + result.getMsgId() + ") " + result.getSendStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 //        System.out.println(result.toString());
-            return "{\"MsgId\":\"" + result.getMsgId() + "\"}";
-        }
+        return "{\"MsgId\":\"" + result.getMsgId() + "\"}";
+    }
 
-        @PreDestroy
-        public void shutDownProducer() {
-            if (producer != null) {
-                producer.shutdown();
-            }
+    @PreDestroy
+    public void shutDownProducer() {
+        if (producer != null) {
+            producer.shutdown();
         }
+    }
 }
